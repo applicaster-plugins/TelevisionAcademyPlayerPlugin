cdd9ab9e16ea20294d69a54f8d3ddc30ddd2b89c
Dmytro Puzak <dmytro.puzak@corewillsoft.com>
2020-01-10 10:02:59 +0200

    Added test URL handling. Release 0.0.6

---
+++ android/ta_player_plugin/src/main/java/com/applicaster/plugin/televisionacademyplayer/ConfigurationRepository.kt
@@ -0,0 +1,15 @@
+package com.applicaster.plugin.televisionacademyplayer
+
+object ConfigurationRepository {
+
+    private val KEY_TEST_VIDEO_URL = "test_video_url"
+
+    var testVideoUrl = ""
+
+    fun parseConfigurationFields(params: Map<*, *>) {
+        params[KEY_TEST_VIDEO_URL]?.let {
+            testVideoUrl = it.toString()
+        }
+    }
+
+}
