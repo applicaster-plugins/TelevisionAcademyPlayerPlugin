5fb568b99585bfd3bf6eea2ad5d0243acf03f9f6
Vlad Sumtsov <vlad.sumtsov@gmail.com>
2020-04-23 08:01:55 +0300

    feature: IM:401 Implement Bitmovin analytics inside the player - iOS

--- ios/TelevisionAcademyIOSPlayerPlugin/Classes/DemoViewController.swift
+++ ios/TelevisionAcademyIOSPlayerPlugin/Classes/DemoViewController.swift
@@ -12,16 +12,20 @@ import ZappPlugins
 class DemoViewController: UIViewController {
     
     @IBOutlet  var inlineView: UIView!
-
+    
     var plugin: BitmovinPlayerPlugin!
     
     @IBAction func buttonDemo() {
-
-        let configurationJSON = ["test_video_url": "https://bitmovin.com/player-content/playhouse-vr/progressive.mp4"] as NSDictionary
-                
+        
+        let configurationJSON = [
+            "test_video_url": "https://bitmovin.com/player-content/playhouse-vr/progressive.mp4",
+            "plist.BitmovinPlayerLicenseKey": "9909b054-c194-416e-b2bc-13f9a781431e",
+            "BitmovinAnalyticLicenseKey": "2ffbbc62-9506-4f27-948f-29c645037b2c"
+            ] as NSDictionary
+        
         let playable = ZPPlayablItem.createVASTVideo()
         plugin = BitmovinPlayerPlugin.pluggablePlayerInit(playableItems: [playable], configurationJSON: configurationJSON) as! BitmovinPlayerPlugin
         plugin.presentPlayerFullScreen(self, configuration: nil, completion: nil)
-//        plugin.pluggablePlayerAddInline(self, container: inlineView)
+        //        plugin.pluggablePlayerAddInline(self, container: inlineView)
     }
 }
