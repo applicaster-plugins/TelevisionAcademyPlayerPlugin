75d99dffac9d63638c8abbdf13ab4a4611a1d102
Barak Halevi <barak@msapps.mobi>
2020-06-01 18:12:13 +0300

    log remove

--- tvos/plugin/android/src/main/kotlin/com/tva/quickbrickplayerplugin/api/VoidCallback.kt
+++ tvos/plugin/android/src/main/kotlin/com/tva/quickbrickplayerplugin/api/VoidCallback.kt
@@ -1,6 +1,5 @@
 package com.tva.quickbrickplayerplugin.api
 
-import android.util.Log
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -9,8 +8,6 @@ import retrofit2.Response
  * Created by Barak Halevi on 01/06/2020.
  */
 class VoidCallback<T> : Callback<T> {
-    override fun onResponse(call: Call<T>, response: Response<T>) {
-        Log.e("barak",response.toString())
-    }
+    override fun onResponse(call: Call<T>, response: Response<T>) {}
     override fun onFailure(call: Call<T>, t: Throwable) {}
 }
